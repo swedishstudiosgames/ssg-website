6297df05f8c8b12f98f0ad7a36a81e5d62d90a73
Lukas Akdogan <lukas.akdogan@gmail.com>
2024-04-04 11:06:11 +0200

    Delete root/lib/src/flip.js
    Signed-off-by: Lukas Akdogan <lukas.akdogan@gmail.com>

--- root/lib/src/flip.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('bt');
-const botMessage = document.getElementById('bm');
-form.addEventListener('submit',(event) => {
-    event.preventDefault();
-    botMessage.textContent = "Silly bot! Go and fuck yourself!";
-    botMessage.style.display = "block";
-});
